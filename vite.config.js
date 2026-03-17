c90fbbf8fcbff6e6c27f4f5074ff1060f709d7b5
Pedro Brito <pedro.brito.flui@gmail.com>
2026-03-17 16:39:36 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
-  base: "/programa-delfin/",
-
   server: {
-    host: "0.0.0.0",
+    host: "0.0.0.0", 
     port: 5173,
     strictPort: true,
     cors: true,
+
     allowedHosts: true
   }
 });
